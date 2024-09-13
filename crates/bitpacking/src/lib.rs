5774a63bb4e587a5544968e0bfaa0bcc7ae3799d
Thomas Böhm <thomas-a.boehm@rohde-schwarz.com>
2024-09-13 07:27:56 +0200

    Implemented fastpack.rs

--- crates/bitpacking/src/lib.rs
+++ crates/bitpacking/src/lib.rs
@@ -1,12 +1,4 @@
-// #![no_std]
-
-#[cfg(test)]
-mod tests;
 
 mod fastpack;
-mod fastunpack;
-mod fastpackwithoutmask;
 
 pub use fastpack::fastpack;
-pub use fastunpack::fastunpack;
-pub use fastpackwithoutmask::fastpackwithoutmask;

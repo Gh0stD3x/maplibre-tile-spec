60dfebf83b78cc8ed0d7098e3fe894797031ca78
Thomas Böhm <thomas-a.boehm@rohde-schwarz.com>
2024-09-23 09:16:27 +0200

    Begin add SIMD to FastPFOR

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
 
-mod arch;
+// mod arch;
+//
+// pub use arch::*;
 
-pub use arch::*;
+pub trait Decoder {
+    fn decode(&mut self, in_buf: &[u32], in_pos: &mut usize, out_buf: &mut [u32], out_pos: &mut usize);
+}
+
+pub trait Encoder {
+    fn encode(&mut self, in_buf: &[u32], in_pos: &mut usize, out_buf: &mut [u32], out_pos: &mut usize);
+}

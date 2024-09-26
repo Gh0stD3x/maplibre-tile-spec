44635f0fc7a18ffe2ab103e157885bd23a98cfbd
Thomas Böhm <thomas-a.boehm@rohde-schwarz.com>
2024-09-26 06:29:14 +0200

    Update Bitpacking

--- crates/bitpacking/src/lib.rs
+++ crates/bitpacking/src/lib.rs
@@ -5,12 +5,20 @@ mod fastpack;
 mod fastunpack;
 mod fastpackwithoutmask;
 
-#[allow(non_snake_case)]
-pub mod BitPacking {
-    use super::fastpack::fastpack as _fastpack;
-    use super::fastunpack::fastunpack as _fastunpack;
-    use super::fastpackwithoutmask::fastpackwithoutmask as _fastpackwithoutmask;
+use fastpack::fastpack as _fastpack;
+use fastunpack::fastunpack as _fastunpack;
+use fastpackwithoutmask::fastpackwithoutmask as _fastpackwithoutmask;
 
+
+pub struct BitPacking {}
+impl BitPacking {
+    pub fn encode(input: &[u32; 32], output: &mut [u32; 32], bit: u8) {
+        _fastpack(input, 0, output, 0, bit);
+    }
+    pub fn decode(input: &[u32; 32], output: &mut [u32; 32], bit: u8) {
+        _fastunpack(input, 0, output, 0, bit);
+    }
+    
     pub fn fastpack(input: &[u32; 32], output: &mut [u32; 32], bit: u8) {
         _fastpack(input, 0, output, 0, bit);
     }
